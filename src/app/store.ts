b47b9cfb4ebc5af0d219442e9023a5249b5478f1
KatIsCoding <fabrigoru@gmail.com>
2022-01-28 18:41:17 -0600

    Store expansion

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
-import { ThunkAction, Action, createStore, applyMiddleware } from '@reduxjs/toolkit';
+import { ThunkAction, Action, createStore, applyMiddleware, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reducer from '../ducks/test/test'
+import dialogHandler from '../ducks/dialog/dialog'
+import people from '../ducks/people/people'
 
 
-export const store = createStore(reducer, applyMiddleware(thunk, logger))
+export const store = createStore(combineReducers({test: reducer, dialogHandler, people: people}), applyMiddleware(thunk, logger))
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
