4d2e6762e9bea756cb94e5a43eb935af5ed93d25
KatIsCoding <fabrigoru@gmail.com>
2022-01-28 18:41:29 -0600

    Header links added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import { AppBar, IconButton, Toolbar, Fab } from '@mui/material'
-import { styled } from '@mui/material/styles'
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import SupervisedUserCircleRoundedIcon from '@mui/icons-material/SupervisedUserCircleRounded';
 import AddBusinessRoundedIcon from '@mui/icons-material/AddBusinessRounded';
+import { Link, useLocation } from 'react-router-dom'
+
 
 const IconStyle = {
   height: '46px',
@@ -10,19 +11,33 @@ const IconStyle = {
 }
 
 
+
 export const Header = () => {
+  const location = useLocation();
+
+  const checkForActive = (path: string) => {
+    return location.pathname === path ? 'white' : 'black';
+  }
+
+  
   return (<header>
         <AppBar position="fixed" sx={{top: 0, bottom: 'auto'}}>
           <Toolbar style={{display: 'flex', justifyContent: 'space-around'}}>
-            <IconButton>
-              <AccountCircleRoundedIcon style={{color: 'black', ...IconStyle}}/>
-            </IconButton>
-            <IconButton>
-              <SupervisedUserCircleRoundedIcon style={{color: 'white', ...IconStyle}}/>
-            </IconButton>
-            <IconButton>
-              <AddBusinessRoundedIcon style={{color: 'black', ...IconStyle}}/>
-            </IconButton>
+            <Link to={'/people'}>
+              <IconButton>
+                <AccountCircleRoundedIcon style={{color: checkForActive('/people'), ...IconStyle}}/>
+              </IconButton>
+            </Link>
+            <Link to={'/'}>
+              <IconButton>
+                <SupervisedUserCircleRoundedIcon style={{color: checkForActive('/'), ...IconStyle}}/>
+              </IconButton>
+            </Link>
+            <Link to={'/properties'}>
+              <IconButton>
+                <AddBusinessRoundedIcon style={{color: checkForActive('/properties'), ...IconStyle}}/>
+              </IconButton>
+            </Link>
           </Toolbar>
         </AppBar>
       </header>)
