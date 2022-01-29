79fff4b5f3bac908eb80430bd7894d35b8991f04
KatIsCoding <fabrigoru@gmail.com>
2022-01-28 18:44:49 -0600

    Routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Pages from './pages';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {Footer} from './components/Footer'
+import {Header} from './components/Header'
 
+//TODO: Add the properties and coincidences routes and pages
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Pages.App />} />
+          <Route path="/people" element={<Pages.People />} />
+        
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
