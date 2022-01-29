db8757f962a817ea220d20a44680674c33ef3110
KatIsCoding <fabrigoru@gmail.com>
2022-01-28 18:40:45 -0600

    App file moved

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { executeTest } from '../ducks/test/test'
+import { RootState } from '../app/store';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
