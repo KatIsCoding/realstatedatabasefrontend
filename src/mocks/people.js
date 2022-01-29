2c09f2e73e4125222d36c65b41c99a43f8ef3246
KatIsCoding <fabrigoru@gmail.com>
2022-01-28 18:45:08 -0600

    Mock data for testing

---
+++ src/mocks/people.js
@@ -0,0 +1,60 @@
+const People = {
+  people: [
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+    {
+      name: "John Snow",
+      phoneNumber: "1234567890",
+      interestedPrice: "100",
+      preferredLocation: "New York",
+      preferredRooms: "1",
+      preferredBathrooms: "1",
+      preferredSize: "100m2",
+    },
+  ]
+}
+
+export default People
